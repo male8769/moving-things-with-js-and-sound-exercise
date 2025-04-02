fd0a61882b1fa65d6cdc5114812f390259528ecb
male8769 <malene_storgaard@live.dk>
2025-04-02 10:43:16 +0200

    keydown

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const dodger = document.getElementById("dodger");
 
 dodger.style.backgroundColor = "#FF69B4";
 
-dodger.style.bottom = "0px";
+//dodger.style.bottom = "0px";
 
-dodger.style.left = "0px";
+//dodger.style.left = "0px";
+
+document.addEventListener("keydown", function (event) {
+    console.log(event);
+  });
