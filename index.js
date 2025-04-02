687e24f949835ca92df7c52a415de5539baa7068
male8769 <malene_storgaard@live.dk>
2025-04-02 10:45:32 +0200

    move left

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ dodger.style.backgroundColor = "#FF69B4";
 
 //dodger.style.left = "0px";
 
+//document.addEventListener("keydown", function (event) {
+//    console.log(event);
+
+//});
+
 document.addEventListener("keydown", function (event) {
-    console.log(event);
+    if (event.key === "ArrowLeft") {
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
+  
+      dodger.style.left = `${left - 1}px`;
+    }
   });
